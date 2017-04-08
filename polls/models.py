98bfa69dd4291ef869b475cd434e06a0e08ab084
Lukáš Dočkal <lukas.dockal@olc.cz>
2017-04-08 16:37:33 +0200

    -- autoprocedure from django and new records

--- polls/models.py
+++ polls/models.py
@@ -1,24 +1,8 @@
-from __future__ import unicode_literals
-import datetime
+# -*- coding: utf-8 -*-
 
+
+from time import timezone
 from django.db import models
-from django.utils import timezone
-
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
-
-def was_published_recently(self):
-    now = timezone.now()
-    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.mail import send_mail
+from django.utils.translation import ugettext_lazy as _
